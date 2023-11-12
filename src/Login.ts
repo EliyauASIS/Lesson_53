9cad16baf61e23806f950924fe2e877aeed5e590
Shiran <shirani1978@walla.com>
2023-11-12 20:39:49 +0200

    chang

---
+++ src/Login.ts
@@ -0,0 +1,19 @@
+
+let username=document.getElementById("username")as HTMLInputElement;
+let password=document.getElementById("password")as HTMLInputElement;
+
+void function login()
+{
+    
+    let usersData = JSON.parse();
+
+   
+    if (username.value == "" || password.value == "" || username.value!=usersData.username || password.value!=usersData.password ) {
+        alert( "Incorrect Data, Please try again!");
+        
+    } 
+    else if (username.value==usersData.username && password.value==usersData.password)
+    {
+                window.location.href = "./homePage.html";   
+    }
+}
