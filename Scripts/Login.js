19f8f39058deb0e3f478717febd0b2dc27073586
Shiran <shirani1978@walla.com>
2023-11-15 15:26:54 +0200

    vfd

--- Scripts/Login.js
+++ Scripts/Login.js
@@ -19,6 +19,7 @@ xhr4.send();
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 function login() {
+<<<<<<< Updated upstream
     for (let x in allUsersArr1) {
         if (username.value == allUsersArr1[x].username && password.value == allUsersArr1[x].password) {
             localStorage.setItem("loginuser", JSON.stringify(allUsersArr1[x].username));
@@ -31,3 +32,17 @@ function login() {
         alert("Incorrect Data, Please try again!");
     }
 }
+=======
+
+if (username.value == "" || password.value == "" || username.value!=allUsersArr1.username || password.value!=allUsersArr1.password ) {
+       alert( "Incorrect Data, Please try again!");
+       
+   }
+   else if (username.value==usersArr.username && password.value==usersArr.password)
+   {
+               window.location.href = "../Pages/Home.html";
+   }
+
+}
+//document.getElementById("logbtn1")?.addEventListener("click",login);
+>>>>>>> Stashed changes
