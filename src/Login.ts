ebb99ee50dcff5390dfd3f1efc6b4e6236c4831e
Shiran <shirani1978@walla.com>
2023-11-15 13:32:10 +0200

    chane user name

--- src/Login.ts
+++ src/Login.ts
@@ -1,6 +1,6 @@
 let allUsersArr1: any = [];
-
- let xhr4 = new XMLHttpRequest();
+let flag =0;
+let xhr4 = new XMLHttpRequest();
 xhr4.open("GET", "../data/users.json", true);
 
 xhr4.onprogress = function () {
@@ -21,15 +21,24 @@ let username=document.getElementById("username")as HTMLInputElement;
 let password=document.getElementById("password")as HTMLInputElement;
 
 function login()
-{   
-    if (username.value == "" || password.value == "" || username.value!=usersArr.username || password.value!=usersArr.password ) {
+{
+    
+    for(let x in allUsersArr1){
+        if(username==allUsersArr1.username && password==allUsersArr1.password)
+            flag=1; 
+    }
+        if(flag==1){
+            console.log("shiran");
+        }
+
+    /*if (username.value == "" || password.value == "" || username.value!=usersArr.username || password.value!=usersArr.password ) {
         alert( "Incorrect Data, Please try again!");
         
     } 
     else if (username.value==usersArr.username && password.value==usersArr.password)
     {
                 window.location.href = "./Home.html";   
-    }
+    }*/
 }
 
 //document.getElementById("logbtn1")?.addEventListener("click",login);
