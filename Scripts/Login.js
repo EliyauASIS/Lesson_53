038101551117d046f490e4837685b32f7e0c7c71
Shiran <shirani1978@walla.com>
2023-11-15 15:26:17 +0200

    df
    dfds

--- Scripts/Login.js
+++ Scripts/Login.js
@@ -10,19 +10,24 @@ xhr4.onload = function () {
     let data = JSON.parse(this.responseText);
     let usersArr = data.users;
     allUsersArr1.push(...usersArr);
+    let newUser = localStorage.getItem("newUser");
+    console.log(newUser);
+    allUsersArr1.push(JSON.parse(newUser));
     console.log(allUsersArr1);
 };
 xhr4.send();
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 function login() {
-    if (username.value == "" || password.value == "" || username.value != allUsersArr1.username || password.value != allUsersArr1.password) {
+    for (let x in allUsersArr1) {
+        if (username.value == allUsersArr1[x].username && password.value == allUsersArr1[x].password) {
+            localStorage.setItem("loginuser", JSON.stringify(allUsersArr1[x].username));
+            localStorage.setItem("Usersarr", JSON.stringify(allUsersArr1));
+            window.location.href = "../Pages/Home.html";
+            flag = 1;
+        }
+    }
+    if (flag == 0) {
         alert("Incorrect Data, Please try again!");
     }
-    else if (username.value == allUsersArr1.username && password.value == allUsersArr1.password) {
-        window.location.href = "./Home.html";
-    }
-     * /;
-    //document.getElementById("logbtn1")?.addEventListener("click",login);
 }
-//document.getElementById("logbtn1")?.addEventListener("click",login);
